6eb80a949f1c75f7872557ee015536312f1ec93b
Dragos Horodnic <dragos@horodnic.com>
2019-03-05 01:31:20 +0200

    Document s.js

--- src/utils/s.js
+++ src/utils/s.js
@@ -1,5 +1,11 @@
 import { h } from 'preact'
 
+/*  This is just an utility that allows declaring classes on components in a 
+    styled components fashion.
+
+    Example:
+    const MyDiv = s('div')`border-box pa3`
+*/
 const s = el => cls => props => h(el, {
     class: cls, ...props
 }, props.children)
