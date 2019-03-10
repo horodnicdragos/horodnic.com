e7398574fbcdc6547d145acaf26587494981210c
Dragos Horodnic <dragos@horodnic.com>
2019-03-10 20:06:04 +0200

    Header focus ring fix

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,11 +2,11 @@ import { Link } from 'preact-router/match'
 
 import s from '../../utils/s'
 
-const Nav = s('nav')`db dt-l w-100 border-box pa3`
+const Nav = s('nav')`db dt-l w-100 border-box pt3`
 const BtnContainer = s('div')`db dtc-l v-mid w-100 w-75-l tc tr-l`
-const Logo = s(Link)`db dtc-l v-mid mid-gray link w-100 w-25-l tc tl-l mb2 mb0-l`
-const NavBtn = e => s(e)`link dark-gray f6 f5-l dib mr3 mr4-l pa2 hover-bb-ns`
-const NavBtnLast = e => s(e)`link dark-gray f6 f5-l dib pa2 hover-b`
+const Logo = s(Link)`db tc tl-l h3`
+const NavBtn = e => s(e)`link pointer f6 f5-m f4-l dib mr4-l mr4-m dim black-80`
+const NavBtnLast = e => s(e)`link pointer f6 f5-m f4-l dib dim black-80`
 
 const isExternal = url => url.match(/^(http|https):\/\//)
 
@@ -20,20 +20,22 @@ const getNavBtnType = (pages, p, i) => {
 const renderNavBtns = pages => pages.map((p, i) => {
   const Btn = getNavBtnType(pages, p, i)
   return (
-    <Btn activeClassName='bb-ns' href={p.route} title={p.title}
-      target={isExternal(p.route) ? '_blank' : null}>{p.title}</Btn>
+    <Btn activeClassName='bb' href={p.route} title={p.title}
+      target={isExternal(p.route) ? '_blank' : null}>
+      <div class="focus__content pa2" tabIndex={-1}>{p.title}</div>
+    </Btn>
   )
 })
 
 const Header = () => (
   <header>
     <Nav>
-      <Logo href="/" title="Home">
-        <img src='assets/logo.svg' width='80'></img>
+      <Logo href="/" title="Logo">
+        <img src='assets/logo.svg' class="h3 o-80" alt="Logo"/>
       </Logo>
       <BtnContainer>
         {renderNavBtns([
-          { title: 'Home', route: '/' },
+          { title: 'About', route: '/' },
           { title: 'Resume', route: '/resume' },
           { title: 'Github', route: 'https://github.com/horodnicdragos' },
           { title: 'Contact', route: '/contact' },
